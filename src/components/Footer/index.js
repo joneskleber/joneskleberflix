0096a77e2a0bab446674c7ca0bb4ee5e34344a56
joneskleber <visao@visao-ol.com.br>
2020-07-27 15:22:06 -0300

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="http://www.visao-ol.com.br/">
+      <img src="../assets/img/Logo-white.png" alt="Logo Jones" />
+      </a>
+      <p>
+        Todos os direitos reservados.
+        {' '}
+        <a href="http://www.visao-ol.com.br/">
+          Visão On-line
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
